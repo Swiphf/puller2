843a553f7d7951ddbf998771ae4b48256ea207d0
Ido Alon <90255849+Swiphf@users.noreply.github.com>
2024-07-22 15:57:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,31 +19,41 @@ sqs = boto3.client('sqs', region_name=REGION_NAME, endpoint_url=ENDPOINTS_URL, a
 s3 = boto3.client('s3', region_name=REGION_NAME, endpoint_url=ENDPOINTS_URL, aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
 
 def process_sqs_messages():
+    app.logger.debug('Starting to process SQS messages')
     while True:
         response = sqs.receive_message(
             QueueUrl=QUEUE_URL,
             MaxNumberOfMessages=1,
             WaitTimeSeconds=10
         )
-
+        app.logger.debug(f'Received response: {response}')
+        
         if 'Messages' in response:
             for message in response['Messages']:
-                body = message['Body'].replace('\n', '').replace('\r', '')
-                data = json.loads(body)                    
-                print(f"Processing message: {data}")
-                
-                # Upload the message to S3
-                s3.put_object(
-                    Bucket=BUCKET_NAME,
-                    Key=f"{data['email_timestream']}.json",
-                    Body=json.dumps(data)
-                )
-                # Delete the message from SQS
-                sqs.delete_message(
-                    QueueUrl=QUEUE_URL,
-                    ReceiptHandle=message['ReceiptHandle']
-                )
+                app.logger.debug(f'Message body: {message["Body"]}')
+                try:
+                    data = json.loads(message['Body'])
+                    app.logger.debug(f'Parsed data: {data}')
+                    
+                    # Upload to S3
+                    s3.put_object(
+                        Bucket=BUCKET_NAME,
+                        Key=f"{data['data']['email_timestream']}.json",
+                        Body=json.dumps(data)
+                    )
+                    app.logger.debug(f'Uploaded data to S3 with key: {data["data"]["email_timestream"]}.json')
 
+                    # Delete message from SQS
+                    sqs.delete_message(
+                        QueueUrl=QUEUE_URL,
+                        ReceiptHandle=message['ReceiptHandle']
+                    )
+                    app.logger.debug('Deleted message from SQS')
+                except Exception as e:
+                    app.logger.error(f'Error processing message: {e}')
+        else:
+            app.logger.debug('No messages found')
+            
 @app.route('/start', methods=['GET'])
 def start_processing():
     process_sqs_messages()
