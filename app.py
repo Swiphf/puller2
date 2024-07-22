14e3b1121092ea64730c6be5e4dcea882c3e08b9
Ido Alon <90255849+Swiphf@users.noreply.github.com>
2024-07-22 20:54:53 +0300

    Dev (#21)
    * Create build-deploy.yaml
    
    * Update build-deploy.yaml
    
    * Update build-deploy.yaml
    
    * Update README.md
    
    * Update build-deploy.yaml
    
    * Update build-deploy.yaml
    
    * Update build-deploy.yaml
    
    * Update build-deploy.yaml
    
    * Update build-deploy.yaml
    
    * Update build-deploy.yaml
    
    * Update build-deploy.yaml
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def process_sqs_messages():
                     # Upload to S3
                     s3.put_object(
                         Bucket=BUCKET_NAME,
-                        Key=f"{data['data']['email_timestream']}.json",
+                        Key=f"{data['email_timestream']}.json",
                         Body=json.dumps(data)
                     )
                     app.logger.debug(f'Uploaded data to S3 with key: {data["email_timestream"]}.json')
